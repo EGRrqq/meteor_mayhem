9f1d0eed1d8df012e95d9458abc93031304ca8db
EGRrqq <egrrqqdev@gmail.com>
2024-12-12 11:46:29 -0500

    feat: node.js server + phaser init

---
+++ server/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+const PORT = 1234;
+
+const rootDir = process.cwd();
+
+app.use(morgan("dev"));
+
+app.use(express.static(rootDir + "/client"));
+
+app.listen(PORT, () => {
+	console.log(`Server listening: http://localhost:${PORT}/`);
+});
