84dfacf41d063f675ebf7ab946e9fc57fd2b0d44
EGRrqq <egrrqqdev@gmail.com>
2024-12-14 13:05:24 -0500

    refactor(client): restructure personData into separate type

--- client/scenes/MainGame.js
+++ client/scenes/MainGame.js
@@ -1,5 +1,10 @@
 import { Scene } from "phaser";
 
+/** @typedef {object} IPersonData
+ * @property {IPoint} pos
+ * @property {ISize} size
+ * */
+
 /** @typedef {"keyW" | "keyS" | "keyD" | "keyA"} TKeysMap */
 /** @typedef {Record<TKeysMap, Phaser.Input.Keyboard.Key | undefined>} TKeys */
 /** @typedef {Record<TKeysMap, () => void>} TKeyActions */
@@ -19,29 +24,33 @@ export class MainGame extends Scene {
 	/** @type {TKeyActions} */
 	keyActions = {
 		keyW: () => {
-			this.personData.y -= 1;
-			this.person?.setY(this.personData.y);
+			this.personData.pos.y -= 1;
+			this.person?.setY(this.personData.pos.y);
 		},
 		keyS: () => {
-			this.personData.y += 1;
-			this.person?.setY(this.personData.y);
+			this.personData.pos.y += 1;
+			this.person?.setY(this.personData.pos.y);
 		},
 		keyA: () => {
-			this.personData.x -= 1;
-			this.person?.setX(this.personData.x);
+			this.personData.pos.x -= 1;
+			this.person?.setX(this.personData.pos.x);
 		},
 		keyD: () => {
-			this.personData.x += 1;
-			this.person?.setX(this.personData.x);
+			this.personData.pos.x += 1;
+			this.person?.setX(this.personData.pos.x);
 		},
 	};
 
-	/** @type { IPoint & ISize  }*/
+	/** @type {IPersonData}*/
 	personData = {
-		x: 250,
-		y: 250,
-		w: 50,
-		h: 50,
+		pos: {
+			x: 250,
+			y: 250,
+		},
+		size: {
+			w: 50,
+			h: 50,
+		},
 	};
 	/** @type {Phaser.GameObjects.Rectangle | null }*/
 	person = null;
@@ -53,10 +62,10 @@ export class MainGame extends Scene {
 	init() {
 		// set person
 		this.person = this.add.rectangle(
-			this.personData.x,
-			this.personData.y,
-			this.personData.w,
-			this.personData.h,
+			this.personData.pos.x,
+			this.personData.pos.y,
+			this.personData.size.w,
+			this.personData.size.h,
 			0x00ff00,
 		);
 
